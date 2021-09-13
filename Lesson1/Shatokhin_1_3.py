474e82315c78e48f4eaa61504ae4f3015c0321d6
kadr <kadr_job@mail.ru>
2021-09-13 14:07:53 +0300

    Упражнение 3

--- Lesson1/Shatokhin_1_3.py
+++ Lesson1/Shatokhin_1_3.py
@@ -1,28 +1,29 @@
 # Реализовать склонение слова «процент» во фразе «N процентов». Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
 
-for elem in range(1,101):
+for elem in range(1, 101):
     rest = elem % 10
-    if (elem) in ([11, 12, 13, 14]):
-        str = 'процентов'
-    elif (rest) in ([1]):
-        str = 'процент'
-    elif (rest) in ([2,3,4]):
-        str = 'процента'
-    else: str ='процентов'
+    if elem in ([11, 12, 13, 14]):
+        name = 'процентов'
+    elif rest in ([1]):
+        name = 'процент'
+    elif rest in ([2, 3, 4]):
+        name = 'процента'
+    else:
+        name = 'процентов'
 
-    print('{} {}'.format(elem,str))
+    print('{} {}'.format(elem, name))
 
 # немного другой вариант
 print('Вариант 2')
 for elem in range(1, 101):
     rest = elem % 10
-    if (rest) in ([1, 2, 3, 4]) and (elem // 10) == 1:
-        str = 'процентов'
-    elif (rest) in ([1]):
-        str = 'процент'
-    elif (rest) in ([2, 3, 4]):
-        str = 'процента'
+    if rest in ([1, 2, 3, 4]) and (elem // 10) == 1:
+        name = 'процентов'
+    elif rest in ([1]):
+        name = 'процент'
+    elif rest in ([2, 3, 4]):
+        name = 'процента'
     else:
-        str = 'процентов'
+        name = 'процентов'
 
-    print('{} {}'.format(elem, str))
+    print('{} {}'.format(elem, name))
